b5b40382dbcf18482b3c177d1cdc751780ff7d65
Alex Sloan <alex.sloan@isocket.com>
2015-06-29 10:25:46 -0600

    take control of http middleware

--- config/http.js
+++ config/http.js
@@ -21,7 +21,7 @@ module.exports.http = {
   *                                                                           *
   ****************************************************************************/
 
-  // middleware: {
+  middleware: {
 
   /***************************************************************************
   *                                                                          *
@@ -30,23 +30,23 @@ module.exports.http = {
   *                                                                          *
   ***************************************************************************/
 
-    // order: [
-    //   'startRequestTimer',
-    //   'cookieParser',
-    //   'session',
-    //   'myRequestLogger',
-    //   'bodyParser',
-    //   'handleBodyParserError',
-    //   'compress',
-    //   'methodOverride',
-    //   'poweredBy',
-    //   '$custom',
-    //   'router',
-    //   'www',
-    //   'favicon',
-    //   '404',
-    //   '500'
-    // ],
+    order: [
+      'responseLogger',
+      'cookieParser',
+      'session',
+      'myRequestLogger',
+      // 'bodyParser',
+      'handleBodyParserError',
+      'compress',
+      'methodOverride',
+      'poweredBy',
+      '$custom',
+      'router',
+      'www',
+      'favicon',
+      '404',
+      '500'
+    ],
 
   /****************************************************************************
   *                                                                           *
@@ -54,11 +54,18 @@ module.exports.http = {
   *                                                                           *
   ****************************************************************************/
 
-    // myRequestLogger: function (req, res, next) {
-    //     console.log("Requested :: ", req.method, req.url);
-    //     return next();
-    // }
+    myRequestLogger: function (req, res, next) {
+      sails.log.info("Requested :: ", req.method, req.url);
+      return next();
+    },
 
+    // Using response-time middleware
+    responseLogger: function (req, res, next) {
+      req.on("end", function() {
+        sails.log.info('Responded :: ', req.method, req.url, ' response time: ' + res.getHeader('X-Response-Time'));
+      });
+      require('response-time')()(req, res, next);
+    },
 
   /***************************************************************************
   *                                                                          *
@@ -69,9 +76,9 @@ module.exports.http = {
   *                                                                          *
   ***************************************************************************/
 
-    // bodyParser: require('skipper')
+    bodyParser: require('skipper')
 
-  // },
+  }
 
   /***************************************************************************
   *                                                                          *
