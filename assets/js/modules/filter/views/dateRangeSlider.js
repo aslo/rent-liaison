40767c224356bc0b921532d004c4c7d5309f8da5
Alex Sloan <alex.sloan@isocket.com>
2015-07-21 19:32:31 -0600

    fix date range filter special accomodations for dates

--- assets/js/modules/filter/views/dateRangeSlider.js
+++ assets/js/modules/filter/views/dateRangeSlider.js
@@ -6,6 +6,13 @@ define([
   return RangeSliderView.extend({
 
     initialize: function() {
+      var self = this;
+      ['min', 'max'].forEach(function(field){
+          if (self.model.has(field) && _.isDate(self.model.get(field))) {
+            self.model.set(field, self.model.get(field).getTime(), { silent: true });
+          }
+      });
+
       RangeSliderView.prototype.initialize.call(this, {
         step: 24 * 60 * 60 * 1000 // use a step size of 1 day (in ms)
       });
